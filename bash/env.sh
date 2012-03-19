d7bb0d3c6b58ca7678a6b17fd479e4ee248ad9f3
Pedro Costa <dev@pfac.info>
2012-03-19 02:21:11 +0000

    Linking environment for Linux

--- bash/env.sh
+++ bash/env.sh
@@ -4,5 +4,15 @@ export LSCOLORS=Dxfxdxcxbxxxxxxxxxxxxx
 
 #	CUDA
 export PATH=$PATH:/usr/local/cuda/bin
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/cuda/lib
-
+if [ "$DYLD_LIBRARY_PATH" ];
+then
+	export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/cuda/lib;
+else
+	export DYLD_LIBRARY_PATH=/usr/local/cuda/lib;
+fi;
+if [ "$LD_LIBRARY_PATH" ];
+then
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib;
+else
+	export LD_LIBRARY_PATH=/usr/local/lib;
+fi;
