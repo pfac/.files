c4dae3ca409bb1a04b73ecd856378113586da621
Pedro Costa <dev@pfac.info>
2012-05-29 17:08:45 +0100

    Trying to shut brew up

--- bash/env.sh
+++ bash/env.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-export PATH=./:~/bin:$PATH
+export PATH=.:~/bin:/usr/local/bin:$PATH
 export LSCOLORS=Dxfxdxcxbxxxxxxxxxxxxx
 
+# add the home lib folder to the default search path
+LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
+
 #	CUDA
-export PATH=$PATH:/usr/local/cuda/bin
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/cuda/lib
+#export PATH=$PATH:/usr/local/cuda/bin
+#export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/cuda/lib
 
