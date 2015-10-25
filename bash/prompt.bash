4482f51a057bf1b4757b969739ba75ed8b32d5b5
Pedro Costa <dev@iampfac.com>
2015-10-25 12:02:16 +0000

    Fixes git prompt coloring and enhances prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -8,22 +8,21 @@ require 'git/prompt'
 
 dotfiles_setup_prompt () {
 	local console_token_color;
-	local git_prompt;
-	local git_status;
+	local prefix;
+	local suffix;
 
 	if [ "$?" == "0" ];
 	then
-		console_token_color="green";
+		console_token_color='green';
 	else
-		console_token_color="red";
+		console_token_color='red';
 	fi;
 
-	git_status=`GIT_PS1_SHOWCOLORHINTS=1 __git_ps1 "%s"`
-	if [ "$git_status" ]; then
-		git_prompt="[$git_status]"
-	fi
 
-	export PS1="\n\u$(mspaint -B -w @)\H:$(mspaint -f magenta -w "\\w")  $git_prompt\n $(mspaint -f $console_token_color -w "\\$") "
+	prefix="\n$(mspaint -b black -f white -w "\\u")$(mspaint -B -b black -f white -w @)$(mspaint -b black -f white "\\H:")$(mspaint -f black '') $(mspaint -f blue -w "\\w")  ";
+
+	export GIT_PS1_SHOWCOLORHINTS=true;
+	__git_ps1 "$prefix" "\n $(mspaint -f $console_token_color -w "\\$") " "[%s]"
 }
 
 PROMPT_COMMAND=dotfiles_setup_prompt
