6b72edab0a248cfe8e82ba4c49fb7a7ed9f1691e
Pedro Costa <dev@iampfac.com>
2020-03-04 16:30:15 +0000

    Add battery widget to Awesome config

--- ansible/roles/awesome/files/rc.lua
+++ ansible/roles/awesome/files/rc.lua
@@ -14,6 +14,8 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
+-- streetturtle/awesome-wm-widgets
+local batteryarc_widget = require("awesome-wm-widgets.batteryarc-widget.batteryarc")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
@@ -204,6 +206,9 @@ awful.screen.connect_for_each_screen(function(s)
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
             wibox.widget.systray(),
+            batteryarc_widget({
+              show_current_level = true,
+            }),
             mytextclock,
             s.mylayoutbox,
         },
